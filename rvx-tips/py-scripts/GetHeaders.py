72067dd1b6580297597af460f742cef56179155c
HomeGravity <168160326+HomeGravity@users.noreply.github.com>
2024-05-27 19:49:24 +0900

    Add files via upload

---
+++ rvx-tips/py-scripts/GetHeaders.py
@@ -0,0 +1,27 @@
+request_headers = {
+    ":authority": "github.com",
+    ":method": "GET",
+    ":path": "/inotia00/revanced-patches",
+    ":scheme": "https",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
+    "Cache-Control": "max-age=0",
+    "Dnt": "1",
+    "Priority": "u=0, i",
+    "Sec-Ch-Ua": "\"Chromium\";v=\"124\", \"Google Chrome\";v=\"124\", \"Not-A.Brand\";v=\"99\"",
+    "Sec-Ch-Ua-Mobile": "?0",
+    "Sec-Ch-Ua-Platform": "\"Windows\"",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "same-origin",
+    "Sec-Fetch-User": "?1",
+    "Sec-Gpc": "1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
+}
+
+
+if __name__ == "__main__":
+    for k, v in request_headers.items():
+        print(k, v)
---
+++ rvx-tips/py-scripts/index.py
@@ -0,0 +1,69 @@
+import json
+from pprint import pprint
+
+def OpenJson(filename):
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+
+# JSON 파일 저장 함수
+def SaveHTML(filename, data):
+    with open(filename, "w", encoding='utf-8') as f:
+        f.write(data)
+        
+html = """
+        <div class="rvx-option">
+            <div class="rvx-option-title">
+                <input type="checkbox" name="rvx-option-title" id="rvx-option-id%s" %s>
+                <label for="rvx-option-id%s" data-original-text="%s">
+                    <span>%s</span>
+                </label>
+            </div>
+
+            <div class="rvx-option-desc">
+                <span>
+                    %s
+                </span>
+            </div>
+            
+            <div class="rvx-option-version">
+                <span>
+                    %s
+                </span>
+            </div>
+        </div>
+        
+        <!-- 구분 -->
+        <!-- 구분 -->
+        """
+                    
+
+new_html = ""
+patches = OpenJson(r"d:\chrome_Downloads\patches (1).json")
+idx = 1
+for name_index in range(len(patches)):
+    if all(item not in patches[name_index]["compatiblePackages"][0]["name"] for item in ["music", "reddit"]):
+        title = patches[name_index]["name"]
+        title = title.strip()
+        desc = patches[name_index]["description"]
+        desc = desc.strip()
+        use = patches[name_index]["use"]
+
+        versions = patches[name_index]["compatiblePackages"][0]["versions"]
+        
+        if versions is not None:
+            versions_str = "%s ~ %s" % (min(versions), max(versions))
+        
+        else:
+            versions_str = "ALL"
+        
+        
+        if use == True:
+            checked = "checked"
+        else:
+            checked = "".strip()
+        
+        new_html += html % (idx, checked, idx, title, title, desc, versions_str) + "\n\n"
+        idx += 1
+
+SaveHTML(r"rvx-tips\rvx-patches\anddea-test.html", new_html)
