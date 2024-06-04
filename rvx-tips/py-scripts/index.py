19e6b537d786ff2585f260b656f814161c06cbd0
HomeGravity <168160326+HomeGravity@users.noreply.github.com>
2024-06-05 00:51:23 +0900

    Add files via upload

--- rvx-tips/py-scripts/index.py
+++ rvx-tips/py-scripts/index.py
@@ -28,8 +28,15 @@ def init_html():
         <div class="patches-web">
             <h4>
                 <span>
-                    <a href="{Patches_website}">패치 내역 확인</a>
-                    <span class="patches-version">{patches_version}</span>
+                    <a href="{Patches_website}">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M0 256C0 209.4 12.5 165.6 34.3 127.1L144.1 318.3C166 357.5 207.9 384 256 384C270.3 384 283.1 381.7 296.8 377.4L220.5 509.6C95.9 492.3 0 385.3 0 256zM365.1 321.6C377.4 302.4 384 279.1 384 256C384 217.8 367.2 183.5 340.7 160H493.4C505.4 189.6 512 222.1 512 256C512 397.4 397.4 511.1 256 512L365.1 321.6zM477.8 128H256C193.1 128 142.3 172.1 130.5 230.7L54.2 98.5C101 38.5 174 0 256 0C350.8 0 433.5 51.5 477.8 128V128zM168 256C168 207.4 207.4 168 256 168C304.6 168 344 207.4 344 256C344 304.6 304.6 344 256 344C207.4 344 168 304.6 168 256z"/></svg>
+                        패치 내역 확인
+                    </a>
+                    <br>
+                    <span class="patches-version">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M48 80a48 48 0 1 1 96 0A48 48 0 1 1 48 80zM0 224c0-17.7 14.3-32 32-32H96c17.7 0 32 14.3 32 32V448h32c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H64V256H32c-17.7 0-32-14.3-32-32z"/></svg>
+                        {patches_version}
+                    </span>
                 </span>
             </h4>
         </div>
@@ -78,6 +85,7 @@ def ImagesInsert():
                 """
 
 
+
 def generate_html(patches, UserName):
     ImgData = OpenJSON(rf"rvx-tips\py-scripts\PatchesSave\{UserName}Patches\{UserName}AddPatchesImage.json")
 
@@ -87,8 +95,7 @@ def generate_html(patches, UserName):
                 <div class="rvx-option-title">
                     <input type="checkbox" name="rvx-option-title" id="rvx-option-id{idx}" {checked}>
                     <span for="rvx-option-id{idx}" data-original-text="{title}">
-                        <span class="id-copy-selection"><a href="#main-{idx}">#main-{idx}</a></span> 
-                        <span class="Label-Text">{title}</span>
+                        <span class="Label-Text">{idx}. {title}</span>
                     </span>
                 </div>
 
@@ -104,6 +111,13 @@ def generate_html(patches, UserName):
                     </span>
                 </div>
             </div>
+            
+            <!-- 복사 아이콘 공간 -->
+            <span class="id-copy-selection">
+                <a href="#main-{idx}">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M208 0H332.1c12.7 0 24.9 5.1 33.9 14.1l67.9 67.9c9 9 14.1 21.2 14.1 33.9V336c0 26.5-21.5 48-48 48H208c-26.5 0-48-21.5-48-48V48c0-26.5 21.5-48 48-48zM48 128h80v64H64V448H256V416h64v48c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V176c0-26.5 21.5-48 48-48z"/></svg>
+                </a>
+            </span> 
                 
             <!-- 사진 공간 -->
             
