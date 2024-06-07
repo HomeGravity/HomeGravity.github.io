dea37eb928f45b67d3fd5f52ca39d8f1c1578a33
HomeGravity <168160326+HomeGravity@users.noreply.github.com>
2024-06-07 16:17:07 +0900

    Add files via upload

--- rvx-tips/py-scripts/InitPatchesImgJSONWrite.py
+++ rvx-tips/py-scripts/InitPatchesImgJSONWrite.py
@@ -1,25 +1,39 @@
 from basic import *
 from pprint import pprint
 
-def AddImage(patches, UserName):
-    ImgDataJSON = {}
+# def AddImage(patches, UserName):
+#     ImgDataJSON = {}
 
-    for patch in patches:
-        if not any(x in patch["compatiblePackages"][0]["name"] for x in ["music", "reddit"]):
-            ImgDataJSON[patch["name"]] = ["../../../bird-8763079_1280.jpg"]
+#     for patch in patches:
+#         if not any(x in patch["compatiblePackages"][0]["name"] for x in ["music", "reddit"]):
+#             ImgDataJSON[patch["name"]] = ["../../../bird-8763079_1280.jpg"]
 
     
 
-    JSONSave(ImgDataJSON, rf"rvx-tips\py-scripts\PatchesSave\{UserName}Patches\{UserName}AddPatchesImage.json")
+#     JSONSave(ImgDataJSON, rf"rvx-tips\py-scripts\PatchesSave\{UserName}Patches\{UserName}AddPatchesImage.json")
 
 
 
-AddImage(
-    OpenJSON(r"rvx-tips\py-scripts\PatchesSave\inotia00Patches\inotia00 - v4.8.6 - patches.json"),
-    "inotia00"
-    )
+# AddImage(
+#     OpenJSON(r"rvx-tips\py-scripts\PatchesSave\inotia00Patches\inotia00 - v4.8.6 - patches.json"),
+#     "inotia00"
+#     )
 
-AddImage(
-    OpenJSON(r"rvx-tips\py-scripts\PatchesSave\anddeaPatches\anddea - v2.226.0-dev.4 - patches.json"),
-    "anddea"
-    )
+# AddImage(
+#     OpenJSON(r"rvx-tips\py-scripts\PatchesSave\anddeaPatches\anddea - v2.226.0-dev.4 - patches.json"),
+#     "anddea"
+#     )
+
+
+def ImagePathCopy():
+    DataRead = OpenJSON(r"rvx-tips\py-scripts\PatchesSave\anddeaPatches\anddeaAddPatchesImage.json")
+    TargetRead = OpenJSON(r"rvx-tips\py-scripts\PatchesSave\inotia00Patches\inotia00AddPatchesImage.json")
+    
+    for key, values in DataRead.items():
+        if key in TargetRead:
+            TargetRead[key] = values
+
+    
+    JSONSave(TargetRead, r"rvx-tips\py-scripts\PatchesSave\inotia00Patches\inotia00AddPatchesImage.json")
+    
+ImagePathCopy()
